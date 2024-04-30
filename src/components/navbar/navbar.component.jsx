1e0ca82438f8a3a3265e67691352588d64cf2bf3
Po Jui Juan <bray851011@gmail.com>
2024-04-30 10:33:06 -0400

    Update Navbar

--- src/components/navbar/navbar.component.jsx
+++ src/components/navbar/navbar.component.jsx
@@ -1,6 +1,6 @@
-import { useState, useEffect } from "react";
 import { Outlet, Link } from "react-router-dom";
 import { Fragment } from "react";
+import { AiOutlineStock } from "react-icons/ai";
 
 import './navbar.styles.scss'
 
@@ -11,20 +11,25 @@ const Navbar = () => {
                 <div className="navigation">
                     <div className="logo-container">
                         <Link className="nav-link" to='/'>
-                            The Stock Spotter
+                            <AiOutlineStock className="logo"/>
+                            <span>Stock Spotter</span>
                         </Link>
                     </div>
                     
                     <div className="links-container">
+                        <div className='v-line'></div>
                         <Link className="nav-link" to='/'>
                             Introduction
                         </Link>
+                        <div className='v-line'></div>
                         <Link className="nav-link" to='/performance' >
                             Performance
                         </Link>
+                        <div className='v-line'></div>
                         <Link className="nav-link" to='/pricing'>
                             Pricing
                         </Link>
+                        <div className='v-line'></div>
                         <Link className="nav-link" to='/auth'>
                             Sign In
                         </Link>
