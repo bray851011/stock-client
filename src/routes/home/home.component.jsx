071ac9bb2d533cbd325e5657344e1de0355c88e2
Po Jui Juan <bray851011@gmail.com>
2024-05-06 22:04:50 -0400

    Update Home Page

--- src/routes/home/home.component.jsx
+++ src/routes/home/home.component.jsx
@@ -11,25 +11,25 @@ const Home = () => {
                 <div className='upside'>
                     <div className='container'>
                         <div className='header'>
-                            <AiOutlineStock style={{ color: 'lightgray', fontSize: '100px' }}/>
+                            <AiOutlineStock className='logo-icon'/>
                             <h1>THE STOCK SPOTTER</h1>
                         </div>
                         <h3>An AI-POWERED Algorithm</h3>
                         <p>Let AI picks the right stocks for you</p>
-
                     </div>
                 </div>
 
                 <div className='downside'>
                     <div className='container'>
                         <div className='left'>
-                            <div style={{ fontSize: '48px' }}>PROFIT</div>
-                            <div style={{ fontSize: '48px' }}>$<CountUp end={100.02} decimals={2} duration={1.5} /></div>
+                            <div className='text'>PROFIT</div>
+                            <div className='text'>$<CountUp end={100.02} decimals={2} duration={1.5} /></div>
                         </div>
                         <div className='v-line'></div>
+                        <div className='h-line'></div>
                         <div className='right'>
-                            <div style={{ fontSize: '48px' }}>ACTIVE USERS</div>
-                            <div style={{ fontSize: '48px' }}><CountUp end={13} duration={2} /></div>
+                            <div className='text'>ACTIVE USERS</div>
+                            <div className='text'><CountUp end={13} duration={2} /></div>
                         </div>
                     </div>
                 </div>
